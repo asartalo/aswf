6cea471eb497cbd7f89e6ddc43ab9026f8c065a4
asartalo <asartalo@projectweb.ph>
2012-05-31 03:42:54 +0800

    Updated composer.json to include test files.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,11 +2,4 @@
 
 $src_path = realpath(__DIR__ . '/../src');
 $vendor_path = realpath(__DIR__ . '/../vendor');
-require_once $vendor_path . '/AsarClassLoader/src/Asar/ClassLoader.php';
-$classLoader = new \Asar\ClassLoader('Asar\Tests', __DIR__);
-$classLoader->register();
-$classLoader = new \Asar\ClassLoader('Asar', $src_path);
-$classLoader->register();
-$classLoader = new \Asar\ClassLoader('Pimple', $vendor_path . '/Pimple/lib');
-$classLoader->register();
-
+require_once $vendor_path . '/autoload.php';
